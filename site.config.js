14c7d5348f7f2b09f6554eed544b8aa07f26e926
sumkyun <70997770+sumkyun@users.noreply.github.com>
2023-08-09 13:38:39 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -55,14 +55,14 @@ const CONFIG = {
     },
   },
   cusdis: {
-    enable: false,
+    enable: true,
     config: {
       host: "https://cusdis.com",
       appid: "", // Embed Code -> data-app-id value
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 30, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
